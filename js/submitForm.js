b03e5fbcc6922a05507b2e810e7c9c3e9f64283e
Elyza Agosta <eja015@bucknell.edu>
2017-06-12 18:17:23 -0400

    Adding everything

---
+++ js/submitForm.js
@@ -0,0 +1,47 @@
+//Make sure everything is correct
+
+
+$(document).ready(function(){
+
+  $('.selectpicker').selectpicker();
+
+
+  $('.order').click(function(){
+    if ($('.fortifiedNum option:selected').val() == 0 && $('.breezerNum option:selected').val() == 0){
+      console.log("need to enter a bike")
+    }
+  
+  var data = {};
+  data.numFortifiedBike = $('.fortifiedNum option:selected').val();
+  data.numBreezerBike = $('.breezerNum option:selected').val();
+  data.summer = false;
+  if($('.summer').is(':checked')){
+    data.summer = true;
+  }
+  console.log($('#startDate').text();
+
+  data.startDate = $('#startDate').val();
+  data.sendDate = $('#endDate').val();
+
+  data.name = $('#name').val();
+  data.email = $('#email').val();
+  data.phone = $('#phone').val();
+  data.age = $('#age').val();
+  data.feet = $('#feet').val();
+  data.inches = $('#inches').val();
+  data.address = $('#street').val() + " "+ $('#city').val() +" "+ $('#zipcode').val();
+
+  $.ajax({
+    type: 'POST',
+    data: JSON.stringify(data),
+        contentType: 'application/json',
+                url: 'http://localhost:3000/endpoint',            
+                success: function(data) {
+                    console.log('success');
+                    console.log(JSON.stringify(data));
+                }
+            });
+
+  });
+
+});
---
+++ main.js
@@ -0,0 +1,72 @@
+//Import the top-level function of express
+var express = require('express');
+
+// Creates an Express application using the top-level function
+var app = express();
+
+// Define port number as 3000
+const port = process.env.PORT || 3000;
+
+// Set path
+var path = require("path");
+
+//Other packages
+var nodemailer = require('nodemailer');
+var bodyParser = require('body-parser')
+
+// Set public route
+app.use('/css', express.static('css'));
+app.use('/js', express.static('js'));
+app.use('/fonts', express.static('fonts'));
+app.use('/img', express.static('img'));
+app.use('/node_modules', express.static('node_modules'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+
+// Routes HTTP GET requests to the specified path "/" with the specified callback function
+app.get('/', function(request, response) {
+  response.sendFile(__dirname+'/index.html');
+});
+
+
+
+app.post('/endpoint', function(req, res){
+  
+  res.send();
+  
+  var transporter = nodemailer.createTransport({
+  service: 'gmail',
+    auth: {
+      user: 'zagsterbikesleasing@gmail.com',
+      pass: 'ZagsterBikes124!'
+    }
+  });
+
+  var mailOptions = {
+    from: 'zagsterbikesleasing@gmail.com',
+    //to: 'bostonbikerentals@gmail.com',
+    to : 'zagsterbikesleasing@gmail.com',
+    subject: 'New Bike Lease',
+    text: JSON.stringify(req.body)
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+});
+
+
+
+
+
+// Make the app listen on port 3000
+app.listen(port, function() {
+  console.log('Server listening on http://localhost:' + port);
+});
